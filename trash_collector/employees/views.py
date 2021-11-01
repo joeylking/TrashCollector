8a7355dac3154497ca82c089f7a8dd75455dfad4
austin sanchez <austinsanchez45@yahoo.com>
2021-11-01 15:01:26 -0500

    Created a path for editting a profile.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -42,3 +42,19 @@ def create(request):
     else:
         return render(request, 'employees/create.html')
 
+def edit_profile(request):
+    logged_in_user = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        zip_from_form = request.POST.get('zip_code')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'logged_in_employee': logged_in_employee
+        }
+        return render(request, 'employees/edit_profile.html', context)
+
